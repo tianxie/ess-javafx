3776ba92f19aace119a209a30e93c4e3732f37a7
Tian Xie <xietian@ehousechina.com>
2018-11-02 18:41:34 +0800

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+
+    public void sendRequest(ActionEvent actionEvent) {
+
+    }
+}
